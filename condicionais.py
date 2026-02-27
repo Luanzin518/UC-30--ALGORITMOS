93dbbaf0a975c803eb939ffd0eacd57c0f8285a1
Luanzin518 <gpsnascimento2022@gmail.com>
2026-02-27 10:13:52 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,22 @@
+nome = input("digite seu nome: ")
+if len(nome) > 5:
+    print("seu nome é grande, ele possui mais de 20 letras")
+print("o seu nome possui{len(nome)} caracteres.")
+
+
+
+valor = int(input("Digite um número: "))
+if valor % 2 == 0:
+ print("o numero é par")
+else:
+   print("O número é ímpar")
+
+   nota = float(input("Digite sua nota"))
+   if nota > 10:
+      print("passou")
+   else:
+      if nota == 7:
+         print("Pode melhorar")
+      else:
+         print("recuperação.")
+
