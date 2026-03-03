472d8f9833d0ac775da9898ff4d5b2a03bbdb875
Luanzin518 <gpsnascimento2022@gmail.com>
2026-03-03 08:21:51 -0300

    Add files via upload

--- questao2.py
+++
@@ -1,5 +0,0 @@
-distancia = int(input("qual foi a distancia:"))
-combustivel = int(input("gastou quanto de combustivel:"))
-
-total = combustivel / distancia
-print(total)
--- questao3.py
+++
@@ -1,6 +0,0 @@
-resposta = input("Você tem uma carteira de motorista? (s/N):")
-
-possui_cnh = resposta.strip().lower() =="S"
-
-print("Valor:", possui_cnh)
-print("Tipo:", type(possui_cnh))
