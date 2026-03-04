95291451d511c50228d9d0575aab73309b10035a
Luanzin518 <gpsnascimento2022@gmail.com>
2026-03-04 08:33:41 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,30 @@
+#LISTAS
+
+nome1 = "ANA" 
+nome2 = "BRUNO"
+nome3 = "CARLOS"
+
+nomes = ["Ana","Bruno","Carlos"]
+print(nomes)
+
+dados = ["Gabriel", 0,1.60, True]
+
+print(dados)
+print(type(dados))
+print(type(dados[0]))
+print(type(dados[1]))
+print(type(dados[2]))
+print(type(dados[3]))
+
+alunos = ["Adriano", "Barbara", "camila"]
+
+print("primeiro",alunos[0])
+print("primeiro", alunos [1])
+print("primeiro", len(alunos))
+print("primeiro", alunos[2])
+print("primeiro", alunos [len(alunos)- 1])
+
+nomes = ["ana", "bruno", "caio"]
+print("original", nomes)
+nomes. append("Daniel")
+print("Atualizado: ",nomes)
